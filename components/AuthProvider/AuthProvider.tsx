7200ce733ead59990160668445c4ebd4c24792fd
vadim_1975 <75vadimalena96@gmail.com>
2025-07-23 15:19:07 +0300

    fix error

--- components/AuthProvider/AuthProvider.tsx
+++ components/AuthProvider/AuthProvider.tsx
@@ -18,7 +18,7 @@ const AuthProvider = ({ children }: Props) => {
     const fetchUser = async () => {
       try {
         const isAuthenticated = await checkSession();
-        if (isAuthenticated.success) {
+        if (isAuthenticated.succss) {
           const user = await getMe();
           if (user) setUser(user);
         } else {
